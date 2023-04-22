8c96af34aab4fdb6bcaf75ff8006be2b2d90d6ff
niko <1209424096@qq.com>
2023-04-22 12:55:47 +0800

    delegate第一次提交

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_2委托
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
